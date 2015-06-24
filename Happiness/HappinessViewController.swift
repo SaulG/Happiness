7c26beb03c173c02fde33e47e960859248f91a77
Saul Gausin <saul.gausin@gmail.com>
2015-06-24 18:05:01 -0500

    Add a smile by drawing it with UIBezierPath

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by saul on 24/06/15.
+//  Copyright (c) 2015 SaulGausin. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController
+{
+}
